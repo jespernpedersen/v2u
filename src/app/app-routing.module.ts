5cb04cf53e4e8d0641e339107feccb6bbf9f8d81
Jesper Pedersen <jeppenp12@gmail.com>
2020-11-30 14:04:05 +0100

    Angular Material installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+// Login Routes
+import { DashboardComponent } from './views/dashboard.component';
+
+// Child Route Dashboard
+
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent,
+    children: [
+      { 
+        path: '', component: DashboardComponent,
+        children: [
+          { path: '', component: DashboardComponent, },
+          { path: 'humidity', component: DashboardComponent },
+        ] },
+        { path: 'classroom', component: DashboardComponent },
+        { path: 'claim', component: DashboardComponent }
+    ]
+  },
+  { path: 'login',   component: DashboardComponent },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/dashboard.module.ts
@@ -0,0 +1,29 @@
+// Angular Modules
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// Main Component
+import { DashboardComponent } from './dashboard.component';
+
+// Subcomponents
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  exports: [
+      
+  ]
+})
+export class DashboardModule { }
