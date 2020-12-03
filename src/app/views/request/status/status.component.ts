658a948f8effdba5661b3ab44bfb9ed5c2429a51
Jesper Pedersen <jeppenp12@gmail.com>
2020-12-03 14:38:12 +0100

    Create flow for status of incrementing votes

--- src/app/views/request/status/status.component.ts
+++ src/app/views/request/status/status.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
     selector: 'view-status',
     templateUrl: './status.component.html',
     styleUrls: ['status.component.css'],
 })
   
-export class StatusComponent {
+export class StatusComponent implements OnInit {
+    
+    constructor(private router: Router) {}
+
     votecount : number = 0;
     totalvotes : number = 4;
+
+    ngOnInit() {
+        this.addVote();  
+    }
+
+    addVote() {   
+        if(this.votecount != this.totalvotes) {     
+            setTimeout(() => { 
+                this.votecount++;
+                this.addVote(); 
+            }, 4000);  
+        }
+        else {
+            // If vote is finished, we redirect
+            this.router.navigateByUrl('/result');
+        }
+    }
 }
