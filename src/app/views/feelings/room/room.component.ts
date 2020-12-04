72a82d21ff9fa724b2691a6248467ea6190a8f58
Jesper Pedersen <jeppenp12@gmail.com>
2020-12-04 12:27:36 +0100

    Changed the list of room buttons to be dynamic from am object and with a for loop

--- src/app/views/feelings/room/room.component.ts
+++ src/app/views/feelings/room/room.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   })
   
   export class RoomFeelingsComponent {
+    // List
+    list = [
+      {
+        id: 0,
+        icon: "local_fire_department",
+        name: "Hot/Cold"
+      },
+      {
+        id: 1,
+        icon: "dry",
+        name: "Too smelly"
+      },
+      {
+        id: 2,
+        icon: "toys",
+        name: "Fresh air", 
+      },
+      {
+        id: 3,
+        icon: "whatshot",
+        name: "Too sweaty", 
+      },
+      {
+        id: 4,
+        icon: "ac_unit",
+        name: "Freezing", 
+      },
+      {
+        id: 5,
+        icon: "waves",
+        name: "Too windy", 
+      },
+      {
+        id: 6,
+        icon: "invert_colors_off",
+        name: "Humid/Dry", 
+      }
+  ];
 
   }
