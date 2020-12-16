4f6cd89bbe894c6f8fcdb9137aa6f34cdd198bf7
unknown <diog0047@edu.easj.dk>
2020-12-16 11:55:19 +0000

    Added animations

--- src/app/views/game/game-dashboard/game-dashboard.component.ts
+++ src/app/views/game/game-dashboard/game-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-game-dashboard',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-dashboard.component.css']
 })
 export class GameDashboardComponent implements OnInit {
+
+  @ViewChild('gameCard') gameCard : ElementRef;
+  @ViewChild('learnCard') learnCard : ElementRef;
 
   level = 1;
   answer = 0;
@@ -40,25 +43,44 @@ export class GameDashboardComponent implements OnInit {
     } else{
 
       if(this.answer >= this.game[this.level-1].answer.min - 3 && this.answer < this.game[this.level-1].answer.min){
-        this.tip = "It is still a <strong>little <span class='blue'>cold</span></strong>";
+         this.AnimateWrong("It is still a <strong>little <span class='blue'>cold</span></strong>");
       } else if( this.answer >= this.game[this.level-1].answer.min - 6 &&  this.answer <= this.game[this.level-1].answer.min-3){
-        this.tip = "It is still <strong><span class='blue'>cold</span></strong>";
+         this.AnimateWrong("It is still <strong><span class='blue'>cold</span></strong>");
       }else if( this.answer <= this.game[this.level-1].answer.min-6){
-        this.tip = "It is still <strong>really <span class='blue'>cold</span></strong>";
+         this.AnimateWrong("It is still <strong>really <span class='blue'>cold</span></strong>");
       } else if( this.answer <= this.game[this.level-1].answer.max + 3 &&  this.answer > this.game[this.level-1].answer.max){
-        this.tip = "It is still <strong>a little <span class='red'>hot</span></strong>";
+        this.AnimateWrong("It is still <strong>a little <span class='red'>hot</span></strong>");
       } else if( this.answer <= this.game[this.level-1].answer.max + 3 &&  this.answer > this.game[this.level-1].answer.max){
-        this.tip = "It is still <strong><span class='red'>hot</span></strong>";
+        this.AnimateWrong("It is still <strong><span class='red'>hot</span></strong>");
       } else {
-        this.tip = "It is still <strong>really <span class='red'>hot</span></strong>";
+        this.AnimateWrong("It is still <strong>really <span class='red'>hot</span></strong>");
       }
-      this.showTip = true;
     }
   }
 
-  NextLevel(){
+  AnimateWrong(tip){
+    let card = document.querySelector("#gameCard");
+    card.classList.add("animate__animated", "animate__shakeX", "animate__fast");
     this.showTip = false;
-    this.isInChallenge = false;
+    card.addEventListener('animationend', () => {
+      card.classList.remove("animate__animated", "animate__shakeX", "animate__fast");
+      this.tip = tip;
+      this.showTip = true;
+    });
+
+  }
+
+  NextLevel(){
+    let card = document.querySelector("#gameCard");
+    card.classList.add("animate__animated", "animate__pulse", "animate__faster");
+    card.addEventListener('animationend', () => {
+      card.classList.remove("animate__pulse", "animate__faster");
+      card.classList.add("animate__fadeOut", "animate__fast");
+      card.addEventListener('animationend', () => {
+        this.showTip = false;
+        this.isInChallenge = false;
+      });
+    });
   }
 
   SetAnswer(answer){
